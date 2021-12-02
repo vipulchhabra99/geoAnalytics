ebf96c6e706261f31c9ff2c94f0a4b5948ae0e39
Tarun Sreepada <rageristic@tutanota.com>
2021-12-03 01:11:45 +0900

    Update csv2raster.py

--- geoAnalytics/csv2raster.py
+++ geoAnalytics/csv2raster.py
@@ -1,70 +1,51 @@
-import numpy as np
 from osgeo import gdal
 import pandas as pd
-import random
-import sys
 import os
+import random
 import subprocess
 
-
-class Csv2raster:
-    """"
-    Usage --> python3 csv2raster.py  inputfile_path
-    """
-
-    def __init__(self, input_file, sep=" "):
-
-        if isinstance(input_file, str):
-            self.dataFrame = pd.read_csv(
-                input_file, sep=" ", header=None, index_col=None)
-        elif isinstance(input_file, pd.DataFrame):
-            self.dataFrame = input_file
-
-        self.band_count = self.dataFrame.shape[1] - 2
-        self.columns = ['x', 'y']
-        for i in range(self.band_count):
-            self.columns.append("b" + str(i + 1))
-        self.dataFrame.columns = self.columns
-        self.dataFrame.to_csv("uneven.csv", index=False)
-
-    def save_raster_image(self, outFile):
-        for i in range(self.band_count):
-            self.output_file = "raster_" + self.columns[i + 2] + "_" + str(random.randint(0, 100000)) + ".nc"
-            f = open("uneven.vrt", "w")
-            f.write("<OGRVRTDataSource>\n \
-                <OGRVRTLayer name=\"uneven\">\n \
-                    <SrcDataSource>uneven.csv</SrcDataSource>\n \
-                    <GeometryType>wkbPoint</GeometryType>\n \
-                    <GeometryField encoding=\"PointFromColumns\" x=\"x\" y=\"y\" z=\"self.columns[i+2]\"/>\n \
-                </OGRVRTLayer>\n \
-                    </OGRVRTDataSource>")
-            f.close()
-            r = gdal.Rasterize(self.output_file, "uneven.vrt",
-                               xRes=1, yRes=-1, attribute=self.columns[i + 2])
-            r = None
-
-            g = gdal.Grid("unevenInt.tif", "uneven.vrt")
-            g = None
-
-            os.remove("uneven.vrt")
-        os.remove("uneven.csv")
-        os.remove("unevenInt.tif")
-        if outFile[-3:] == '.nc':
-            buffer = 'cdo cat raster_b*.nc ' + outFile
-            print(subprocess.getstatusoutput(buffer))
-            buffer = 'rm raster_*'
-            print(subprocess.getstatusoutput(buffer))
-        elif outFile[-3:] == 'iff' or outFile[-3:] == 'tif':
-            buffer = 'cdo cat raster_b*.nc combined.nc'
-            print(subprocess.getstatusoutput(buffer))
-            buffer = 'gdal_translate -of GTiff combined.nc ' + outFile
-            print(subprocess.getstatusoutput(buffer))
-            buffer = 'rm raster_*'
-            print(subprocess.getstatusoutput(buffer))
-            buffer = 'rm combined.nc'
-            print(subprocess.getstatusoutput(buffer))
+def csv2raster(input_file='', output_file='output.nc', sep=" ", dataframe=''):
+    input_file = input_file
+    inputfile_sep = sep
+    tempOut = output_file
+    output_file = 'output.nc'
+    dataFrame = dataframe
+    if input_file != '':
+        dataFrame = pd.read_csv(input_file, 
+                                     sep=inputfile_sep, 
+                                     header=None, index_col=None)
 
 
-if __name__ == "__main__":
-    csv2raster = Csv2raster(input_file=sys.argv[1])
-    csv2raster.save_raster_image()
+    dataFrame = dataFrame.sort_values(['y','x'], ascending=[True,True])
+    dataFrameColumns = dataFrame.columns
+
+    randInt = str(random.randint(0, 100000))
+    dataFrame = dataFrame.sort_values(by=['y', 'x'], ascending = [False,True])
+
+    for i in range(2,len(dataFrameColumns)):
+        dataFrame.to_csv("xyzformat.xyz", index=False, header=None, sep=" ")
+        raster = gdal.Translate("temp_" + randInt + "_" + str(dataFrameColumns[i]) + ".nc", "xyzformat.xyz")
+        dataFrame = dataFrame.drop(columns=dataFrameColumns[i])
+        buffer = 'ncrename -v ' + 'Band1,' + str(dataFrameColumns[i]) + " temp_" + randInt + "_" + str(dataFrameColumns[i]) + ".nc" 
+        print(subprocess.getstatusoutput(buffer))
+
+
+    buffer = 'cdo -f nc2 cat ' + "temp_" + randInt + "_*.nc " + output_file
+    print(subprocess.getstatusoutput(buffer))
+
+    buffer = 'rm ' + "temp_" + randInt + "_*.nc"
+    print(subprocess.getstatusoutput(buffer))
+    
+    if tempOut[-3:] == '.nc':
+        #rename
+        os.rename(output_file, tempOut)
+    elif tempOut[-3:] == 'iff' or tempOut[-3:] == 'tif':
+        buffer = 'gdal_translate -of GTiff output.nc ' + tempOut
+        print(subprocess.getstatusoutput(buffer))
+        buffer = 'rm output.nc'
+        print(subprocess.getstatusoutput(buffer))
+    else:
+        os.rename(output_file, tempOut + ".nc")
+
+
+    os.remove("xyzformat.xyz")
