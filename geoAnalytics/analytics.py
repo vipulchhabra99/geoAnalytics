f43da45f24e1fdb57423cc19d1f44e189cbd045c
Tarun Sreepada <rageristic@tutanota.com>
2021-12-17 11:26:43 +0900

    Update analytics.py

--- geoAnalytics/analytics.py
+++ geoAnalytics/analytics.py
@@ -1,25 +1,36 @@
-import os
-import subprocess
-import psycopg2
 import pandas as pd
-from config import config
+import numpy as np
 
-class analytics:
-                    
-    def KNN(tableName, X,Y,Bands,k = 1000,SRID=4326):
-        conn = None
-        try:
-            # read database configuration
-            params = config()
-            # connect to the PostgreSQL database
-            conn = psycopg2.connect(**params)
-            # create a new cursor
-            cur = conn.cursor()
-            query = "SELECT "+ Bands + " FROM " + tableName + " ORDER BY " + tableName + ".geom <-> 'SRID=" + str(SRID) + ";POINT(" + str(X) + ' ' + str(Y) + ")'::geometry limit" + str(k) + ";"
-            dataFrameKNN = pd.read_sql_query(query, conn)
-            return dataFrameKNN
-        except (Exception, psycopg2.DatabaseError) as error:
-            print(error)
-        finally:
-            if conn is not None:
-                conn.close()
+def cosineSimilarity(tableName1, tableName2):
+    df1 = tableName1
+    df2 = tableName2
+
+    cosineSimilarityDF = pd.DataFrame(columns=['X', 'Y', 'Cosine Similarity'])
+
+    # get x and y
+    x = df1['x']
+    y = df1['y']
+
+    # drop x and y
+    df1 = df1.drop(['x', 'y'], axis=1)
+    df2 = df2.drop(['x', 'y'], axis=1)
+
+    # convert to numpy array
+    df1 = df1.to_numpy()
+    df2 = df2.to_numpy()
+
+    # calculate cosine similarity
+
+    # for each row in df2
+    for i in range(len(df2)):
+        similarity_scores = df1.dot(df2)/ (np.linalg.norm(df1, axis=1) * np.linalg.norm(df2))
+        # get average of similarity scores
+        average_similarity_score = np.average(similarity_scores)
+        # append to dataframe
+        cosineSimilarityDF = cosineSimilarityDF.append({'X': x[i], 'Y': y[i], 'Cosine Similarity': average_similarity_score}, ignore_index=True)
+    
+    return cosineSimilarityDF
+    
+
+
+
